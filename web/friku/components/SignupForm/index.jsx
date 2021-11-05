b105560f0a907867ecea0b611a0abd52ce8b0e5d
cyobrin@gmail.com <cyobrin@gmail.com>
2021-11-06 01:26:46 +0900

    Create pages/signup Prototype

---
+++ web/friku/components/SignupForm/index.jsx
@@ -0,0 +1,292 @@
+import { useState } from "react";
+
+// Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+
+export default function SignupForm(props) {
+  const { handleSubmit, onSubmit, register, errors, watch } = props;
+  const watchPassword = watch("password");
+  const [isRevealPassword, setIsRevealPassword] = useState(false);
+  const [isRevealPasswordConfirm, setIsRevealPasswordConfirm] = useState(false);
+
+  const setYear = () => {
+    const years = [];
+    for (let i = 1900; i <= new Date().getFullYear(); i++) {
+      years.push(
+        <option key={`year_${i}`} value={i}>
+          {i}
+        </option>
+      );
+    }
+    return years;
+  };
+
+  const setMonth = () => {
+    const months = [];
+    for (let i = 1; i <= 12; i++) {
+      months.push(
+        <option key={`month_${i}`} value={i}>
+          {i}
+        </option>
+      );
+    }
+    return months;
+  };
+
+  const setDay = () => {
+    const days = [];
+    for (let i = 1; i <= 31; i++) {
+      days.push(
+        <option key={`month_${i}`} value={i}>
+          {i}
+        </option>
+      );
+    }
+    return days;
+  };
+
+  const togglePassword = () => {
+    setIsRevealPassword((prevState) => !prevState);
+  };
+
+  const togglePasswordConfirm = () => {
+    setIsRevealPasswordConfirm((prevState) => !prevState);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <ul className="w-full steps mb-10">
+          <li className="step step-primary">必要事項入力</li>
+          <li className="step">入力内容確認</li>
+          <li className="step">登録完了</li>
+        </ul>
+        <div className="w-full md:w-1/2 px-3 mb-6">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="lastName"
+          >
+            姓
+          </label>
+          <input
+            className="appearance-none block w-full text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:bg-gray-200"
+            id="lastName"
+            type="text"
+            placeholder="姓"
+            {...register("lastName", {
+              required: true,
+              pattern: /^[^0-9]+$/,
+            })}
+          />
+          {errors.lastName?.type === "required" && (
+            <p className="text-red-500 text-xs italic">姓は必須です</p>
+          )}
+          {errors.lastName?.type === "pattern" && (
+            <p className="text-red-500 text-xs italic">数字は入力できません</p>
+          )}
+        </div>
+        <div className="w-full md:w-1/2 px-3 mb-6">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="firstName"
+          >
+            名
+          </label>
+          <input
+            className="appearance-none block w-full text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:bg-gray-200"
+            id="firstName"
+            type="text"
+            placeholder="名"
+            {...register("firstName", { required: true, pattern: /^[^0-9]+$/ })}
+          />
+          {errors.firstName?.type === "required" && (
+            <p className="text-red-500 text-xs italic">名は必須です</p>
+          )}
+          {errors.firstName?.type === "pattern" && (
+            <p className="text-red-500 text-xs italic">数字は入力できません</p>
+          )}
+        </div>
+        <div className="w-full px-3 mb-6">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="email"
+          >
+            メールアドレス
+          </label>
+          <input
+            className="appearance-none block w-full text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:bg-gray-200"
+            id="email"
+            type="email"
+            placeholder="your@email.com"
+            {...register("email", { required: true })}
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500 text-xs italic">
+              メールアドレスは必須です
+            </p>
+          )}
+        </div>
+        <div className="w-full px-3 mb-6">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="gender"
+          >
+            生年月日
+          </label>
+          <div className="w-full flex">
+            <select
+              id="birthYear"
+              name="birthYear"
+              {...register("birthYear")}
+              className="select block w-1/4 text-gray-700 border border-gray-500 rounded cursor-default py-3 px-4 mb-3 leading-tight focus:bg-gray-200"
+            >
+              {setYear()}
+            </select>
+            <span className="w-1/12 text-center inline-block align-bottom">
+              年
+            </span>
+            <select
+              id="birthMonth"
+              name="birthMonth"
+              {...register("birthMonth")}
+              className="select block w-1/4 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:bg-gray-200"
+            >
+              {setMonth()}
+            </select>
+            <span className="w-1/12 text-center inline-block align-bottom">
+              月
+            </span>
+            <select
+              id="birthDay"
+              name="birthDay"
+              {...register("birthDay")}
+              className="select block w-1/4 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:bg-gray-200"
+            >
+              {setDay()}
+            </select>
+            <span className="w-1/12 text-center inline-block align-bottom">
+              日
+            </span>
+          </div>
+        </div>
+        <div className="w-full px-3 mb-6">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-6"
+            htmlFor="gender"
+          >
+            性別
+          </label>
+          <input
+            type="radio"
+            name="gender"
+            value={1}
+            className="radio radio-xs"
+            {...register("gender", { required: true })}
+          />
+          <span className="ml-2 mr-5">男性</span>
+          <input
+            type="radio"
+            name="gender"
+            value={2}
+            className="radio radio-xs"
+            {...register("gender", { required: true })}
+          />
+          <span className="ml-2">女性</span>
+          {errors.gender?.type === "required" && (
+            <p className="text-red-500 text-xs italic">性別は必須です</p>
+          )}
+        </div>
+        <div className="w-full md:w-1/2 px-3 mb-6 relative">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="password"
+          >
+            パスワード
+          </label>
+          <div className="relative">
+            <input
+              className="appearance-none block w-full text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:bg-gray-200"
+              id="password"
+              type={isRevealPassword ? "text" : "password"}
+              placeholder="パスワード"
+              {...register("password", {
+                required: true,
+                pattern: /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,100}$/,
+              })}
+            />
+            <div
+              onClick={togglePassword}
+              role="presentation"
+              className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
+            >
+              {isRevealPassword ? (
+                <FontAwesomeIcon icon={faEye} className="hover:text-primary" />
+              ) : (
+                <FontAwesomeIcon
+                  icon={faEyeSlash}
+                  className="hover:text-primary"
+                />
+              )}
+            </div>
+          </div>
+
+          {errors.password?.type === "required" && (
+            <p className="text-red-500 text-xs italic">必須です</p>
+          )}
+          {errors.password?.type === "pattern" && (
+            <p className="text-red-500 text-xs italic">
+              8文字以上の半角英数字（大文字小文字数字を各1文字ずつ含む）で入力してください
+            </p>
+          )}
+        </div>
+        <div className="w-full md:w-1/2 px-3 mb-6">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="passwordConfirm"
+          >
+            パスワード（確認用）
+          </label>
+          <div className="relative">
+            <input
+              className="appearance-none block w-full text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:bg-gray-200"
+              id="passwordConfirm"
+              type={isRevealPasswordConfirm ? "text" : "password"}
+              placeholder="パスワード（確認用）"
+              {...register("passwordConfirm", {
+                required: "パスワード（確認用）は必須です",
+                validate: (value) =>
+                  value === watchPassword || "パスワードが一致していません",
+              })}
+            />
+            <div
+              onClick={togglePasswordConfirm}
+              role="presentation"
+              className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
+            >
+              {isRevealPasswordConfirm ? (
+                <FontAwesomeIcon icon={faEye} className="hover:text-primary" />
+              ) : (
+                <FontAwesomeIcon
+                  icon={faEyeSlash}
+                  className="hover:text-primary"
+                />
+              )}
+            </div>
+          </div>
+          {errors.passwordConfirm && (
+            <p className="text-red-500 text-xs italic">
+              {errors.passwordConfirm.message}
+            </p>
+          )}
+        </div>
+      </div>
+      <button
+        className="btn btn-primary border-0 hover:shadow px-8 rounded-xl w-full"
+        type="submit"
+      >
+        入力内容確認
+      </button>
+    </form>
+  );
+}
---
+++ web/friku/pages/signup/index.jsx
@@ -0,0 +1,95 @@
+import Link from "next/link";
+import { useForm, Controller } from "react-hook-form";
+import { useContext, useState } from "react";
+import { useRouter } from "next/router";
+
+// Components
+import SignupForm from "../../components/SignupForm";
+import SignupConfirmation from "../../components/SignupConfirmation";
+
+// Contexts
+import { AuthContext } from "../../contexts/Auth/index";
+
+const signUp = () => {
+  const [formData, setFormData] = useState();
+  const [isConfirmed, setIsConfirmed] = useState(false);
+  const { user, signUp } = useContext(AuthContext);
+  const router = useRouter();
+  const {
+    control,
+    register,
+    formState: { errors },
+    handleSubmit,
+    watch,
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    setFormData({ ...data, gender: parseInt(data.gender) });
+    setIsConfirmed(true);
+  };
+
+  const onClickBack = () => {
+    setIsConfirmed(false);
+  };
+
+  const onClickSignup = async (data) => {
+    const res = await signUp(data);
+    if (res) {
+      router.push("/signup/success");
+      return;
+    }
+    alert("会員登録失敗しました");
+  };
+
+  if (user) router.push("/");
+
+  return (
+    <div className="bg-gradient-to-b from-primary to-secondary">
+      <div className="container mx-auto">
+        <div className="flex justify-center px-6 py-14">
+          <div
+            className="w-full h-auto hidden bg-white lg:block lg:w-1/2 rounded-l-lg"
+            style={{
+              backgroundImage: "url(" + "/images/signup.svg" + ")",
+              backgroundPosition: "bottom",
+              backgroundSize: "550px",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <div className="w-full lg:w-1/2 bg-white p-20 rounded-lg lg:rounded-l-none">
+            <div className="text-center mb-12">
+              <h1 className="mt-6 text-5xl font-bold text-gray-900">SIGN UP</h1>
+              <p className="mt-2 text-sm text-gray-500">
+                必要項目入力の上、入力内容確認へ進んでください。
+              </p>
+              <Link href="/signin">
+                <a className="mt-2 text-sm text-indigo-500 hover:text-gray-500">
+                  すでに登録済みの方はこちらからログイン
+                </a>
+              </Link>
+            </div>
+            {isConfirmed ? (
+              <SignupConfirmation
+                formData={formData}
+                onClickBack={onClickBack}
+                signup={onClickSignup}
+              />
+            ) : (
+              <SignupForm
+                Controller={Controller}
+                control={control}
+                handleSubmit={handleSubmit}
+                onSubmit={onSubmit}
+                register={register}
+                errors={errors}
+                watch={watch}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default signUp;
