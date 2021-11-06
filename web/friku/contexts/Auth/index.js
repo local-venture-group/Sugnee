733de2655adf7991796eef78ff3c0388eadf6596
cyobrin@gmail.com <cyobrin@gmail.com>
2021-11-06 23:09:38 +0900

    Create pages/login

--- web/friku/contexts/Auth/index.js
+++ web/friku/contexts/Auth/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
+import Router from "next/router";
 
 const AuthContext = createContext(null);
 
@@ -23,7 +24,7 @@ const AuthProvider = ({ children }) => {
       });
   };
 
-  const signUp = (data) => {
+  const signup = (data) => {
     const {
       firstName,
       lastName,
@@ -49,7 +50,6 @@ const AuthProvider = ({ children }) => {
         console.log(res);
         if (res.data) {
           console.log("[signup]会員登録成功");
-          return res;
         } else {
           console.log(res.data.statusText);
           console.log("[signup]会員登録失敗");
@@ -59,14 +59,63 @@ const AuthProvider = ({ children }) => {
         console.log(err.response);
         console.log("[signup]会員登録失敗");
       });
-    return resData;
+  };
+
+  const login = (data) => {
+    const { email, password } = data;
+    axios
+      .get("http://localhost/sanctum/csrf-cookie", { withCredentials: true })
+      .then(() => {
+        axios
+          .post(
+            "http://localhost/api/user/login",
+            {
+              email,
+              password,
+            },
+            { withCredentials: true }
+          )
+          .then((res) => {
+            if (res.status === 200) {
+              setUser(res.data.user);
+              Router.push("/");
+            } else {
+              console.log(res.data);
+              alert("[login]ログイン失敗");
+            }
+          })
+          .catch((err) => {
+            console.log(err.response);
+            console.log("[login]ログイン失敗");
+          });
+      });
+  };
+
+  const logout = () => {
+    axios
+      .post("http://localhost/api/user/logout")
+      .then((res) => {
+        if (res.status === 200) {
+          alert("ログアウトしました");
+          setUser(null);
+        } else {
+          console.log(res.data);
+          console.log("[logout]ログアウト失敗");
+        }
+      })
+      .catch((err) => {
+        console.log(err.response);
+        console.log("[login]ログイン失敗");
+      });
   };
 
   return (
     <AuthContext.Provider
       value={{
         user,
-        signUp,
+        signup,
+        login,
+        logout,
       }}
     >
       {children}
--- web/friku/pages/index.jsx
+++ web/friku/pages/index.jsx
@@ -4,12 +4,17 @@ import { useContext } from "react";
 import { AuthContext } from "../contexts/Auth";
 
 export default function Home() {
-  const { user } = useContext(AuthContext);
+  const { user, logout } = useContext(AuthContext);
   console.log("test", user);
   return (
     <div className="container mx-auto">
       <main className="w-full h-96 bg-gradient-to-b from-primary to-secondary">
         <h1>Fリク</h1>
+        {user && (
+          <button type="button" className="btn" onClick={logout}>
+            ログアウト
+          </button>
+        )}
       </main>
     </div>
   );
