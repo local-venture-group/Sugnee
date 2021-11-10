5457577594322d47b7616ff40579cb51075bf572
cyobrin@gmail.com <cyobrin@gmail.com>
2021-11-10 17:16:24 +0900

    Create component/Layout

--- web/friku/pages/_app.jsx
+++ web/friku/pages/_app.jsx
@@ -6,12 +6,17 @@ import { AuthProvider } from "../contexts/Auth";
 import { AdminProvider } from "../contexts/Admin";
 import { StaffProvider } from "../contexts/Staff";
 
+// Components
+import Layout from "../components/Layout";
+
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <AdminProvider>
         <StaffProvider>
-          <Component {...pageProps} />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
         </StaffProvider>
       </AdminProvider>
     </AuthProvider>
