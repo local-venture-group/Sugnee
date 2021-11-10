98477c27baa9c8358bfc9cf1f33dc456f8f084c2
cyobrin@gmail.com <cyobrin@gmail.com>
2021-11-10 17:35:19 +0900

    Remove logoutButton from index

--- web/friku/pages/index.jsx
+++ web/friku/pages/index.jsx
@@ -1,20 +1,8 @@
-import { useContext } from "react";
-
-// テスト用
-import { AuthContext } from "../contexts/Auth";
-
 export default function Home() {
-  const { user, logout } = useContext(AuthContext);
-
   return (
     <div className="container mx-auto">
       <main className="w-full h-96 bg-gradient-to-b from-primary to-secondary">
         <h1>Fリク</h1>
-        {user && (
-          <button type="button" className="btn" onClick={logout}>
-            ログアウト
-          </button>
-        )}
       </main>
     </div>
   );
