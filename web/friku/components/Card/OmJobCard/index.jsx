0acc0ce350a7956bb50733f3d3ab704cb4cbd113
cyobrin@gmail.com <cyobrin@gmail.com>
2021-11-10 18:33:36 +0900

    Add NewJobsList in index

---
+++ web/friku/components/Card/OmJobCard/index.jsx
@@ -0,0 +1,85 @@
+import { useContext } from "react";
+import Link from "next/link";
+import { handleDate, isFavorite } from "../../../utils";
+
+// Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMapMarkerAlt,
+  faYenSign,
+  faBookmark as faBookmarked,
+} from "@fortawesome/free-solid-svg-icons";
+import { faBookmark, faClock } from "@fortawesome/free-regular-svg-icons";
+
+// Contexts
+import { AuthContext } from "../../../contexts/Auth";
+
+// Components
+import BookmarkButton from "../../BookmarkButton";
+
+export default function OmJobCard({ job, user, userFavorites }) {
+  const { addOmBookmark, deleteOmBookmark } = useContext(AuthContext);
+
+  return (
+    <Link href={`/jobOffer/job/${job.id}`}>
+      <div className="card shadow-lg lg:card-side w-full bg-white hover:shadow cursor-pointer">
+        <div className="card-body">
+          <div className="card-title flex justify-between">
+            <p
+              className={`badge ${
+                job.hiring_system === "正社員"
+                  ? "badge-success"
+                  : "badge-warning"
+              } p-3 mb-3`}
+            >
+              {job.hiring_system ? job.hiring_system : "なし"}
+            </p>
+            {user && userFavorites && isFavorite(userFavorites, job.id) ? (
+              <BookmarkButton
+                text={<FontAwesomeIcon icon={faBookmarked} />}
+                color={"pink"}
+                event={(e) => deleteOmBookmark(e, user, job.id)}
+              />
+            ) : (
+              <BookmarkButton
+                text={<FontAwesomeIcon icon={faBookmark} />}
+                event={(e) => addOmBookmark(e, user, job.id)}
+              />
+            )}
+          </div>
+          <p>{job.company_name}</p>
+          <p className="text-xl text-primary h-10">{job.job_type}</p>
+          <ul className="mt-6">
+            <li>
+              <FontAwesomeIcon icon={faMapMarkerAlt} />
+              <span className="text-gray-500 text-sm ml-3">{job.city}</span>
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faYenSign} />
+              <span className="text-gray-500 text-sm ml-3">
+                {job.salary_pattern}
+                {job.salary_min}円〜{job.salary_max}円
+              </span>
+            </li>
+          </ul>
+          <p className="mt-3 text-sm text-gray-500">
+            {job.job_description.substring(0, 45)}・・・
+          </p>
+          {/* <div className="justify-end card-actions">
+            {userFavorites && isFavorite(userFavorites, job.id) ? (
+              <BookmarkButton text={<FontAwesomeIcon icon={faBookmarked} />} />
+            ) : (
+              <BookmarkButton text={<FontAwesomeIcon icon={faBookmark} />} />
+            )}
+          </div> */}
+          <div className="justify-end card-actions text-xs text-gray-400">
+            <p>
+              <FontAwesomeIcon icon={faClock} />
+              <span className="ml-1">{handleDate(job.created_at)}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- web/friku/pages/index.jsx
+++ web/friku/pages/index.jsx
@@ -1,9 +1,68 @@
-export default function Home() {
+import { useContext, useRef } from "react";
+import { useRouter } from "next/router";
+
+import axios from "axios";
+
+// Contexts
+import { AuthContext } from "../contexts/Auth";
+
+// Components
+import OmJobCard from "../components/Card/OmJobCard";
+
+// Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faKey, faStar } from "@fortawesome/free-solid-svg-icons";
+import { faLightbulb } from "@fortawesome/free-regular-svg-icons";
+
+export default function Home({ omJobs }) {
+  const router = useRouter();
+  const { user } = useContext(AuthContext);
+  const userFavorites = user?.favorites.map(
+    (favoriteJob) => favoriteJob.corporation_joboffer_id
+  );
   return (
-    <div className="container mx-auto">
-      <main className="w-full h-96 bg-gradient-to-b from-primary to-secondary">
-        <h1>Fリク</h1>
-      </main>
+    <div>
+      <section className="w-full h-96 bg-gradient-to-b from-primary to-secondary"></section>
+
+      <section className="flex flex-col justify-center items-center w-full px-4 pt-10 bg-gray-50">
+        <FontAwesomeIcon
+          icon={faLightbulb}
+          className="text-accent mb-2"
+          size="3x"
+        />
+        <h1 className="text-3xl">PickUp求人</h1>
+      </section>
+      <section className="flex flex-col justify-center items-center w-full px-4 pt-10">
+        <FontAwesomeIcon icon={faStar} className="text-accent mb-2" size="3x" />
+        <h1 className="text-3xl">特集一覧</h1>
+      </section>
+      <section className="flex flex-col justify-center items-center w-full px-4 pt-10 bg-gray-50">
+        <FontAwesomeIcon
+          icon={faLightbulb}
+          className="text-accent mb-2"
+          size="3x"
+        />
+        <h1 className="text-3xl">新着求人</h1>
+
+        <div className="w-full my-10 p-4 space-x-4 carousel carousel-center rounded-box">
+          {omJobs.map((job) => (
+            <div className="carousel-item md:w-1/3 w-full" key={job.id}>
+              <OmJobCard job={job} user={user} userFavorites={userFavorites} />
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const omJobs = await axios
+    .get("http://nginx:80/api/user/top")
+    .then((res) => res.data)
+    .catch((err) => console.log(err));
+
+  return {
+    props: { omJobs },
+  };
 }
