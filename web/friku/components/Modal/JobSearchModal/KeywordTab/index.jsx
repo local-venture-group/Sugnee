9dc5414ab2a4a24314f85bb362b34601bcb636cf
cyobrin@gmail.com <cyobrin@gmail.com>
2021-11-11 17:50:11 +0900

    Fix component/KeywordTab Layout

--- web/friku/components/Modal/JobSearchModal/KeywordTab/index.jsx
+++ web/friku/components/Modal/JobSearchModal/KeywordTab/index.jsx
@@ -5,15 +5,14 @@ import { faKey } from "@fortawesome/free-solid-svg-icons";
 export default function KeywordTab({ register }) {
   return (
     <div className="w-full">
-      <div className="relative w-10/12 left-1/2 transform -translate-x-1/2">
-        <input
-          type="text"
-          className="w-full rounded-full shadow absolute -top-8 h-16 z-10 p-6 focus:border-0 focus:bg-gray-100"
-          placeholder="フリーワードで検索"
-          {...register("keywords")}
-        />
-      </div>
-      <div className="container w-10/12 mx-auto mt-16">
+      <input
+        type="text"
+        className="w-full rounded-full shadow h-16 p-6 focus:border-0 focus:bg-gray-100"
+        placeholder="フリーワードで検索"
+        {...register("keywords")}
+      />
+
+      <div className="container mt-8">
         <p>
           <FontAwesomeIcon icon={faKey} size="lg" className="text-primary" />
           人気の検索ワード
