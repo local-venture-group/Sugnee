63bdb00d58a6863696b1dc229637393f7e4f6bdc
cyobrin@gmail.com <cyobrin@gmail.com>
2021-11-11 17:00:45 +0900

    Create component/JobSearchModal

---
+++ web/friku/components/Modal/JobSearchModal/KeywordTab/index.jsx
@@ -0,0 +1,32 @@
+// Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faKey } from "@fortawesome/free-solid-svg-icons";
+
+export default function KeywordTab({ register }) {
+  return (
+    <div className="w-full">
+      <div className="relative w-10/12 left-1/2 transform -translate-x-1/2">
+        <input
+          type="text"
+          className="w-full rounded-full shadow absolute -top-8 h-16 z-10 p-6 focus:border-0 focus:bg-gray-100"
+          placeholder="フリーワードで検索"
+          {...register("keywords")}
+        />
+      </div>
+      <div className="container w-10/12 mx-auto mt-16">
+        <p>
+          <FontAwesomeIcon icon={faKey} size="lg" className="text-primary" />
+          人気の検索ワード
+        </p>
+
+        <button
+          type="button"
+          value="フレックス"
+          className="text-sm bg-blue-100 px-6 py-2 m-3 rounded-full hover:bg-blue-200"
+        >
+          フレックス
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ web/friku/components/Modal/JobSearchModal/WorkTypeTab/index.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+
+// Context
+import { SearchConditionContext } from "../../../../contexts/SearchCondition";
+
+export default function WorkTypeTab({ register }) {
+  const { workTypes } = useContext(SearchConditionContext);
+  if (!workTypes) return null;
+
+  return (
+    <div className="w-1/2">
+      {workTypes.map((type, i) => (
+        <div key={i}>
+          <label className="cursor-pointer label justify-start">
+            <input
+              id={`${type === "勤務地" ? "location" : "workType"}-${i}`}
+              type="checkbox"
+              className="checkbox checkbox-sm"
+              value={i}
+              {...register("workType")}
+            />
+            <span className="text-sm ml-3">{type}</span>
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+}
