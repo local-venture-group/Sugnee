9e9e2acb6c02446fc2b42663a3625e5d7a5e5e45
cyobrin@gmail.com <cyobrin@gmail.com>
2021-11-16 15:48:26 +0900

    Add HeroSection to pages/job

--- web/friku/pages/job/[id].jsx
+++ web/friku/pages/job/[id].jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import axios from "axios";
 
 import { handleDate } from "../../utils";
@@ -10,112 +11,128 @@ import {
   faClock,
 } from "@fortawesome/free-solid-svg-icons";
 
-export default function pickUpJobOffer({ job }) {
+export default function job({ job }) {
   return (
     <>
-      <div className="container mx-auto mt-10 px-8 md:px-28">
-        <div className="w-full flex">
-          <div className="w-full lg:w-3/4">
-            <h2 className="text-lg text-primary font-bold">
-              {job.company_name}
-            </h2>
-            <h1 className="text-2xl font-bold">{job.job_type}</h1>
-            <ul className="my-6">
-              <li>
+      <section id="hero">
+        <p className="my-10 ml-10 text-xs text-gray-500">
+          <Link href="/">TOP</Link>
+          <span className="ml-2">&gt;</span>
+          <span className="ml-2">{job.company_name}</span>
+        </p>
+        <div className="hero h-96 bg-gradient-to-b from-primary to-secondary"></div>
+      </section>
+      <section id="body">
+        <div className="container mx-auto mt-10 px-8 md:px-28">
+          <div className="w-full flex">
+            <div className="w-full lg:w-3/4">
+              <h2 className="text-lg text-primary font-bold">
+                {job.company_name}
+              </h2>
+              <h1 className="text-2xl font-bold">{job.job_type}</h1>
+              <ul className="my-6">
+                <li>
+                  <FontAwesomeIcon
+                    icon={faMapMarkerAlt}
+                    className="text-gray-500"
+                  />
+                  <span className="text-gray-500 ml-3">{job.city}</span>
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faYenSign} className="text-gray-500" />
+                  <span className="text-gray-500 ml-3">
+                    {job.salary_min}円〜{job.salary_max}円
+                  </span>
+                </li>
+              </ul>
+              <p className="text-sm">
                 <FontAwesomeIcon
-                  icon={faMapMarkerAlt}
+                  icon={faClock}
+                  size="sm"
                   className="text-gray-500"
                 />
-                <span className="text-gray-500 ml-3">{job.city}</span>
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faYenSign} className="text-gray-500" />
-                <span className="text-gray-500 ml-3">
-                  {job.salary_min}円〜{job.salary_max}円
+                <span className="text-gray-400 ml-3">
+                  {handleDate(job.created_at)}
                 </span>
-              </li>
-            </ul>
-            <p className="text-sm">
-              <FontAwesomeIcon
-                icon={faClock}
-                size="sm"
-                className="text-gray-500"
-              />
-              <span className="text-gray-400 ml-3">
-                {handleDate(job.created_at)}
-              </span>
-            </p>
+              </p>
+            </div>
+            <div className="hidden lg:flex flex-col justify-end w-1/4">
+              <button className="btn btn-outline btn-primary w-3/4 mb-4">
+                お気に入りに追加
+              </button>
+              <button className="btn btn-primary w-3/4">応募する</button>
+            </div>
           </div>
-          <div className="hidden lg:flex flex-col justify-end w-1/4">
-            <button className="btn btn-outline btn-primary w-3/4 mb-4">
-              お気に入りに追加
-            </button>
-            <button className="btn btn-primary w-3/4">応募する</button>
-          </div>
-        </div>
 
-        <p className="bg-primary text-white px-2 py-3 w-full mt-10">仕事内容</p>
-        <p className="mt-8">{job.job_description}</p>
-        <p className="bg-primary text-white px-2 py-3 w-full mt-10">募集要項</p>
-        <div className="border mt-8 p-8">
-          <table className="table-fixed w-full mt-8 break-all">
-            <tbody>
-              <tr className="border-b border-gray-300">
-                <th className="w-1/4 text-left pl-4">職種</th>
-                <td className="w-3/4 p-8">{job.job_type}</td>
-              </tr>
-              <tr className="border-b border-gray-300">
-                <th className="w-1/4 text-left pl-4">雇用形態</th>
-                <td className="w-3/4 p-8">{job.hiring_system}</td>
-              </tr>
-              <tr className="border-b border-gray-300">
-                <th className="w-1/4 text-left pl-4">給与</th>
-                <td className="w-3/4 p-8">
-                  {job.salary_pattern}
-                  {job.salary_min}円〜{job.salary_max}円
-                </td>
-              </tr>
-              <tr className="border-b border-gray-300">
-                <th className="w-1/4 text-left pl-4">勤務地</th>
-                <td className="w-3/4 p-8">
-                  {job.zip_code}
-                  <br />
-                  福岡県{job.city}
-                  {job.building}
-                </td>
-              </tr>
-              <tr className="border-b border-gray-300">
-                <th className="w-1/4 text-left pl-4">勤務時間</th>
-                <td className="w-3/4 p-8">{job.work_time}</td>
-              </tr>
-              <tr className="border-b border-gray-300">
-                <th className="w-1/4 text-left pl-4">休日・休暇</th>
-                <td className="w-3/4 p-8 ">{job.holiday_description}</td>
-              </tr>
-              <tr className="border-b border-gray-300">
-                <th className="w-1/4 text-left pl-4">交通費</th>
-                <td className="w-3/4 p-8">
-                  {job.travel_cost === "なし"
-                    ? "支給なし"
-                    : job.travel_cost_description}
-                </td>
-              </tr>
-              <tr>
-                <th className="w-1/4 text-left pl-4">試用・研修</th>
-                <td className="w-3/4 p-8">
-                  {job.trial}
-                  <br />
-                  {job.trial_period}
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <p className="bg-primary text-white px-2 py-3 w-full mt-10">
+            仕事内容
+          </p>
+          <p className="mt-8">{job.job_description}</p>
+          <p className="bg-primary text-white px-2 py-3 w-full mt-10">
+            募集要項
+          </p>
+          <div className="border mt-8 p-8">
+            <table className="table-fixed w-full mt-8 break-all">
+              <tbody>
+                <tr className="border-b border-gray-300">
+                  <th className="w-1/4 text-left pl-4">職種</th>
+                  <td className="w-3/4 p-8">{job.job_type}</td>
+                </tr>
+                <tr className="border-b border-gray-300">
+                  <th className="w-1/4 text-left pl-4">雇用形態</th>
+                  <td className="w-3/4 p-8">{job.hiring_system}</td>
+                </tr>
+                <tr className="border-b border-gray-300">
+                  <th className="w-1/4 text-left pl-4">給与</th>
+                  <td className="w-3/4 p-8">
+                    {job.salary_pattern}
+                    {job.salary_min}円〜{job.salary_max}円
+                  </td>
+                </tr>
+                <tr className="border-b border-gray-300">
+                  <th className="w-1/4 text-left pl-4">勤務地</th>
+                  <td className="w-3/4 p-8">
+                    {job.zip_code}
+                    <br />
+                    福岡県{job.city}
+                    {job.building}
+                  </td>
+                </tr>
+                <tr className="border-b border-gray-300">
+                  <th className="w-1/4 text-left pl-4">勤務時間</th>
+                  <td className="w-3/4 p-8">{job.work_time}</td>
+                </tr>
+                <tr className="border-b border-gray-300">
+                  <th className="w-1/4 text-left pl-4">休日・休暇</th>
+                  <td className="w-3/4 p-8 ">{job.holiday_description}</td>
+                </tr>
+                <tr className="border-b border-gray-300">
+                  <th className="w-1/4 text-left pl-4">交通費</th>
+                  <td className="w-3/4 p-8">
+                    {job.travel_cost === "なし"
+                      ? "支給なし"
+                      : job.travel_cost_description}
+                  </td>
+                </tr>
+                <tr>
+                  <th className="w-1/4 text-left pl-4">試用・研修</th>
+                  <td className="w-3/4 p-8">
+                    {job.trial}
+                    <br />
+                    {job.trial_period}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
-      <div className="lg:hidden bottom-0 fixed w-full flex justify-center bg-white bg-opacity-90 px-3 pt-8 pb-2">
-        <button className="btn btn-outline w-2/5 mr-2">お気に入りに追加</button>
-        <button className="btn w-2/5">応募する</button>
-      </div>
+        <div className="lg:hidden bottom-0 fixed w-full flex justify-center bg-white bg-opacity-90 px-3 pt-8 pb-2">
+          <button className="btn btn-outline w-2/5 mr-2">
+            お気に入りに追加
+          </button>
+          <button className="btn w-2/5">応募する</button>
+        </div>
+      </section>
     </>
   );
 }
