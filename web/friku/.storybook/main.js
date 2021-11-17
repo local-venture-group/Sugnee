2cd70cd85f850400981c0b21d181b150fe87a1eb
cyobrin@gmail.com <cyobrin@gmail.com>
2021-11-17 18:08:03 +0900

    Install storybook/postcss-addon

--- web/friku/.storybook/main.js
+++ web/friku/.storybook/main.js
@@ -1,13 +1,21 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  core: {
+    builder: "webpack5",
+  },
+};
