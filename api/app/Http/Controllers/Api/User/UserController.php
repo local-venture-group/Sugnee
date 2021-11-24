d48a9255175f9262e8da74214a9aebd7a4cb766c
cyobrin@gmail.com <cyobrin@gmail.com>
2021-11-24 19:17:00 +0900

    Fix UserController/update

--- api/app/Http/Controllers/Api/User/UserController.php
+++ api/app/Http/Controllers/Api/User/UserController.php
@@ -90,7 +90,7 @@ class UserController extends Controller
     public function update(UserUpdateRequest $request, User $user, ImageService $imageService)
     {
 
-
+        $filePath = $user->img_path;
         $folderName = 'users';
         if($request->has('imageBase64')){
             $request->validate([
