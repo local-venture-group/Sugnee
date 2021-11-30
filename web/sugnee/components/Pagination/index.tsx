d3c1a1417d5ec116df22d225ee01ede2dccb5437
cyobrin@gmail.com <cyobrin@gmail.com>
2021-11-30 17:03:56 +0900

    Add previous&next to pagination

--- web/sugnee/components/Pagination/index.tsx
+++ web/sugnee/components/Pagination/index.tsx
@@ -1,16 +1,34 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
+// Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronRight,
+  faChevronLeft,
+} from "@fortawesome/free-solid-svg-icons";
+
 const Pagination: React.FC<{ totalCount: number }> = ({ totalCount }) => {
   const router = useRouter();
   const pageNum = Number(router.query.page);
   const PER_PAGE = 12;
+  const lastPageNum = Math.ceil(totalCount / PER_PAGE);
   const range = (start, end) =>
     [...Array(end - start + 1)].map((_, i) => start + i);
 
   return (
-    <ul className="flex justify-center">
-      {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
+    <ul className="flex justify-center items-center">
+      {pageNum === 1 ? (
+        <FontAwesomeIcon icon={faChevronLeft} className="mr-3" size="2x" />
+      ) : (
+        <Link href={`/pickup/page/${pageNum - 1}`}>
+          <a>
+            <FontAwesomeIcon icon={faChevronLeft} className="mr-3" size="2x" />
+          </a>
+        </Link>
+      )}
+
+      {range(1, lastPageNum).map((number, index) => (
         <li key={index} className="mr-1">
           {pageNum === number ? (
             <button className="btn btn-primary">{number}</button>
@@ -21,6 +39,15 @@ const Pagination: React.FC<{ totalCount: number }> = ({ totalCount }) => {
           )}
         </li>
       ))}
+      {pageNum === lastPageNum ? (
+        <FontAwesomeIcon icon={faChevronRight} className="ml-3" size="2x" />
+      ) : (
+        <Link href={`/pickup/page/${pageNum + 1}`}>
+          <a>
+            <FontAwesomeIcon icon={faChevronRight} className="ml-3" size="2x" />
+          </a>
+        </Link>
+      )}
     </ul>
   );
 };
