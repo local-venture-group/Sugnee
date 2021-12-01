90572e1b3306010b391bff6437c2e75ac29ff23b
cyobrin@gmail.com <cyobrin@gmail.com>
2021-12-01 21:11:36 +0900

    Change siteName

--- web/sugnee/components/Layout/index.tsx
+++ web/sugnee/components/Layout/index.tsx
@@ -1,9 +1,11 @@
 import Header from "../Header";
 import Footer from "../Footer";
+import Seo from "../Seo";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
+      <Seo />
       <div className="bg-white">
         <Header />
         <div className="container-lg mx-auto">{children}</div>
